dc91e72026fdcecf91c086ca2b095c33cef6d687
kulinich denis <denys.kulinich@qvantel.com>
2019-09-19 11:56:26 +0300

    update package and add test for form;

--- test/App.test.js
+++ test/App.test.js
@@ -1,6 +1,11 @@
 import React from "react";
 import App from "../src/app/App";
+import { act } from "react-dom/test-utils";
 import { mount, shallow } from "enzyme";
+import * as validation from "../src/app/utils/validate";
+import { delay } from "../src/app/utils/utils";
+
+validation.validIBAN = jest.fn(() => Promise.resolve(true));
 
 describe("App component", () => {
     let wrapper;
@@ -11,17 +16,33 @@ describe("App component", () => {
         const button = wrapper.find("button").length;
         const form = wrapper.find("form").length;
         expect(form).toBe(1);
+        expect(button).toBe(1);
     });
     it("submit value from form", async () => {
         wrapper.find('input[name="name"]').simulate("blur");
         wrapper.find('input[name="email"]').simulate("blur");
-        act(() => {
-            wrapper.find('input[name="lastname"]').simulate("blur");
-          });
-        //await new Promise((res) => setTimeout(res, 4000));
-        expect(wrapper.find('.is-invalid').length).toBe(2);
+        await act(async() => {
+           wrapper.find('input[name="lastname"]').simulate("blur");
+        });
+        act(() => { wrapper.update();});
+        expect(wrapper.find('.is-invalid').length).toBe(3);
     });
-    it("test async validation", () => {
-
+    it("test async validation", async() => {
+        const name = wrapper.find('input[name="name"]').simulate('change', { target: { value: "Denys", name: "name"}});
+        name.simulate('blur');
+        expect(wrapper.find('input[name="name"]').props().value).toEqual("Denys");
+        expect(wrapper.find('.is-valid').length).toBe(1);
+        
+        const email = wrapper.find('input[name="email"]').simulate('change', { target: { value: "test@test.com", name: "email"}});
+        email.simulate('blur');
+        expect(wrapper.find('input[name="email"]').props().value).toEqual("test@test.com");
+    
+        const lastName = wrapper.find('input[name="lastname"]').simulate('change', { target: { value: "Kulinich", name: "lastname"}});
+        await act(async () => {
+            lastName.simulate('blur');    
+        });
+        expect(wrapper.find('input[name="lastname"]').props().value).toEqual("Kulinich");
+        wrapper.update();
+        expect(wrapper.find('.is-valid').length).toBe(3);
     });
 })
